542afb2f669d297340328e76a12dde154b16efcc
Nicholas Kamimura <nickkami@hotmail.com>
2019-04-26 17:06:38 -0300

    PArtial commit Carousel

--- src/components/Carousel/__stories__/carousel.jsx
+++ src/components/Carousel/__stories__/carousel.jsx
@@ -1,26 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 // import Carousel from '../index';
-import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import { Carousel } from 'react-responsive-carousel';
+import ProductCarousel from '../index';
 
 const stories = storiesOf('Carousel', module);
 
 stories.add('Default', () => {
-    return (
-        <Carousel>
-            <div>
-                <img src="http://react-responsive-carousel.js.org/assets/2.jpeg" />
-                <p className="legend">Legend 1</p>
-            </div>
-            <div>
-                <img src="http://react-responsive-carousel.js.org/assets/2.jpeg" />
-                <p className="legend">Legend 2</p>
-            </div>
-            <div>
-                <img src="http://react-responsive-carousel.js.org/assets/2.jpeg" />
-                <p className="legend">Legend 3</p>
-            </div>
-        </Carousel>
-    );
+    return <ProductCarousel />;
 });
--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -1,3 +1,57 @@
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
+import Rating from '../Rating';
+import styled from 'styled-components';
+
+const TopBox = styled.div`
+    text-align: left;
+`;
+const ContainerCarousel = styled.div`
+    li.slide {
+        background-color: #fff;
+    }
+`;
+const Boximage = styled.div`
+    margin-right: 20px;
+    margin-left: 20px;
+`;
+const carrousel = new Array(10).fill();
+const carouselItens = (id, url, alt) => {
+    [
+        { id: '1', url: 'https://react-responsive-carousel.js.org/assets/1.jpeg', alt: '_' },
+        { id: '2', url: 'https://react-responsive-carousel.js.org/assets/2.jpeg', alt: '_' },
+        { id: '3', url: 'https://react-responsive-carousel.js.org/assets/3.jpeg', alt: '_' },
+    ];
+};
+const imageUrl = 'https://react-responsive-carousel.js.org/assets/3.jpeg';
+const itemsPerPage = 3;
+const BoxCarousel = () => {
+    return (
+        <fragment>
+            <TopBox>
+                <Rating />
+            </TopBox>
+            <Boximage>
+                <img src={imageUrl} />
+            </Boximage>
+        </fragment>
+    );
+};
+const ProductCarousel = () => {
+    return (
+        <ContainerCarousel>
+            <Carousel
+                showThumbs={false}
+                centerMode
+                centerSlidePercentage={100 / itemsPerPage}
+                centerMode={true}
+            >
+                {carrousel.map((item, index) => (
+                    <BoxCarousel />
+                ))}
+            </Carousel>
+        </ContainerCarousel>
+    );
+};
+export default ProductCarousel;
