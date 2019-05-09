918486cb00a8977b952d9d932f13de62bdfa5f4b
nick <nickkami@hotmail.com>
2019-05-08 21:58:00 -0300

    PAtial commit carousel

--- src/components/Carousel/index.jsx
+++ src/components/Carousel/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 import Rating from '../Rating';
+import Thumbnail from '../Thumbnail';
 import styled from 'styled-components';
 
 const TopBox = styled.div`
@@ -18,6 +19,28 @@ const Boximage = styled.div`
 `;
 // const carrousel = new Array(10).fill();
 const itemsPerPage = 3;
+const product = {
+    id: 2,
+    title: 'Handmade Soft Sausages',
+    qty: 18,
+    price: '352.00',
+    special_price: '300.00',
+    rating: 4,
+    short_description: 'Repellendus temporibus veniam esse.',
+    description:
+        'Et ullam ratione quo autem nostrum tempore modi aut. Veniam ad et sequi optio molestiae. Non deserunt sed qui ut.',
+    thumbnail: 'http://lorempixel.com/640/480',
+    gallery: [
+        'http://lorempixel.com/640/480',
+        'http://lorempixel.com/640/480',
+        'http://lorempixel.com/640/480',
+        'http://lorempixel.com/640/480',
+        'http://lorempixel.com/640/480',
+        'http://lorempixel.com/640/480',
+    ],
+    categories: [5],
+    related: [34, 102, 101, 441, 214, 260, 476],
+};
 
 const ProductCarousel = props => {
     return (
@@ -30,11 +53,10 @@ const ProductCarousel = props => {
             >
                 {props.products.map(item => (
                     <div>
-                        <TopBox>
-                            <Rating />
-                        </TopBox>
+                        <TopBox />
                         <Boximage>
-                            <img key={item.id} src={item.url} alt={item.alt} />
+                            <Thumbnail product={product} />
+                            {/* <img key={item.id} src={item.url} alt={item.alt} />  */}
                         </Boximage>
                     </div>
                 ))}
